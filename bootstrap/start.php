2325200129420370e4b7a31c8e9f7a9abb8b05f1
Christian Korndörfer <info@chko.org>
2015-02-13 14:17:13 +0100

    added new local

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead','christiansmbp2','Christians-MacBook-Pro-2.local'),
+	'local' => array('homestead','christiansmbp2','Christians-MacBook-Pro-2.local','christians-mbp'),
 
 ));
 
